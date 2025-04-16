3bae1f75a1f18fe9a490b88103739bd13a8c7d88
Alan Tanui <tanuialan@gmail.com>
2025-04-17 01:23:47 +0300

    Modified - Added the names of Employees

--- employeeage.py
+++ employeeage.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_num_employees():
     return int(input('Enter the number of Employees :'))
 
@@ -10,8 +12,14 @@ for i in range(number):
 
 agetotal = sum(ages)
 
+names = []
+for i in range(number):
+    name = str(input('Enter the Names of the Employees :')).strip().title()
+    names.append(name)
+
+print(f'Name : {names} -- Age {ages}')
 print(f'Total age : {agetotal}')
 print(f'Average age : {round(agetotal / number)}')
-       
+ 
 
 
