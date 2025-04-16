1eb7c6a9df045899d574740b25a46a678cbb409e
Alan-Tanui <tanuialan@gmail.com>
2025-04-17 00:48:55 +0300

    Update employeeage.py - capitalized the names

--- employeeage.py
+++ employeeage.py
@@ -1,11 +1,11 @@
 def get_num_employees():
-    return int(input('Enter the number of employees :'))
+    return int(input('Enter the number of Employees :'))
 
 number = get_num_employees()
 
 ages = []
 for i in range(number):
-    age = int(input(f'Enter the age of employees {i + 1}: '))
+    age = int(input(f'Enter the age of Employees {i + 1}: '))
     ages.append(age)
 
 agetotal = sum(ages)
