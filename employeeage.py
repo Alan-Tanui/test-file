538d486563ee17b8a6269ccfd7967e3719238169
Alan Tanui <tanuialan@gmail.com>
2025-04-17 02:04:39 +0300

    Massive change - Updated the program to give a more desired output and added error check for number of employees

--- employeeage.py
+++ employeeage.py
@@ -5,21 +5,31 @@ def get_num_employees():
 
 number = get_num_employees()
 
-ages = []
-for i in range(number):
-    age = int(input(f'Enter the age of Employees {i + 1}: '))
-    ages.append(age)
+if number < 0 :
+    print('Please Enter a Valid Number!!')
 
-agetotal = sum(ages)
+else :
+    ages = []
+    for i in range(number):
+        age = int(input(f'Enter the age of Employees {i + 1}: '))
+        ages.append(age)
+   
 
-names = []
-for i in range(number):
-    name = str(input('Enter the Names of the Employees :')).strip().title()
-    names.append(name)
+    agetotal = sum(ages)
 
-print(f'Name : {names} -- Age {ages}')
-print(f'Total age : {agetotal}')
-print(f'Average age : {round(agetotal / number)}')
+    names = []
+    for i in range(number):
+        name = str(input('Enter the Names of the Employees :')).strip().title()
+        names.append(name)
+
+    print('\n ---Employee Details ---')     
+    for i in range(number):
+        print(f'Name : {names[i]} -- Age {ages[i]}')
+
+    print('\n --- Statistics ---')
+    print(f'Total number of Employees : {number}') 
+    print(f'Total age : {agetotal}')
+    print(f'Average age : {round(agetotal / number)}')
  
 
 
